b1d212c9c0bd029e0b037782d375391028cfa8a9
dino16m <anselem16m@gmail.com>
2019-12-05 11:31:08 +0100

    yandex verification

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="yandex-verification" content="df12aac9d02782f4" />
   <title>Tefem</title>
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
